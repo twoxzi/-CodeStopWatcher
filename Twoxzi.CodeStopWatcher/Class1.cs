92e7dd9888744b05d0d48fb51e837e54ebcf7502
Soda Twoxzi <twoxzi@foxmail.com>
2019-12-10 12:33:21 +0800

    添加项目文件。

---
+++ Twoxzi.CodeStopWatcher/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twoxzi.CodeStopWatcher
+{
+    public class Class1
+    {
+    }
+}
